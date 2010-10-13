0dbcf07181482ae374ff72f7407d33a814f8a7eb
Gib Bogle <g.bogle@auckland.ac.nz>
2010-10-14 09:55:11 +1300

    Added time display, plots

---
+++ GUI/misc.h
@@ -0,0 +1,51 @@
+#ifndef MISC_H
+#define MISC_H
+
+#include <QThread>
+#include <QTcpServer>
+
+#include "libbone.h"
+
+class SocketHandler : public QThread
+ {
+    Q_OBJECT
+
+public:
+    SocketHandler(int newport, QObject *parent = 0);
+    ~SocketHandler();
+    void run();
+	void end();
+
+	QTcpServer *tcpServer;
+	int port;
+	bool exiting;
+	quint16 blockSize;
+	QTcpSocket *socket;
+	char msg[2048];
+	static const int CPORT0 = 5000;
+	static const int CPORT1 = 5001;
+
+private slots:
+	 void processor();
+signals:
+	 void sh_connected();
+	 void sh_disconnected();
+	 void sh_output(QString);
+	 void sh_error(QString);
+};
+
+class ExecThread: public QThread
+{
+	Q_OBJECT
+	QString inputFile;
+	QString dll_path;
+public:
+//	ExecThread::ExecThread(QString, QString);
+        ExecThread(QString, QString);
+        void run();
+	int ncpu;
+};
+
+bool quitMessage(QString);
+
+#endif
